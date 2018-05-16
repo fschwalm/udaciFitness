edb4ddce0aa6f7ab613a435f3102ceadf6ec7623
Fagner <fagnerschwalm@gmail.com>
2018-05-16 19:16:33 -0300

    Add utils

--- App.js
+++ App.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { AppRegistry, Text, TextInput, View } from "react-native";
+import AddEntry from "./components/AddEntry";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{ padding: 10 }}>
+      
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
