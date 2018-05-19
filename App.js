1c8f8ec28355f974b075cc42d1e34b8f6b51b25f
Fagner <fagnerschwalm@gmail.com>
2018-05-18 21:34:16 -0300

    setup redux

--- App.js
+++ App.js
@@ -2,13 +2,19 @@ import React from "react";
 import { AppRegistry, Text, TextInput, View } from "react-native";
 import AddEntry from "./components/AddEntry";
 import { Ionicons } from '@expo/vector-icons';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import entries from './store/reducers';
+
 
 export default class App extends React.Component {
   render() {
     return (
+      <Provider store={createStore(entries)}>
       <View>
         <AddEntry />
       </View>
+      </Provider>
     );
   }
 }
