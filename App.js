6b0ada46bea4eea35ca7fd1efb46fdedb23c4c33
Fagner <fagnerschwalm@gmail.com>
2018-05-23 21:58:04 -0300

    Add TabNavigator

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
 import React from "react";
-import { AppRegistry, Text, TextInput, View } from "react-native";
+import { AppRegistry, Text, TextInput, View, Platform } from "react-native";
 import AddEntry from "./components/AddEntry";
-import { Ionicons } from '@expo/vector-icons';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux'
-import entries from './store/reducers';
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import entries from "./store/reducers";
 import History from "./components/History";
+import { purple, white } from "./utils/colors";
+import { createBottomTabNavigator } from 'react-navigation'
 
+const Tabs = createBottomTabNavigator(
+  {
+    History: {
+      screen: History,
+      navigationOptions: {
+        tabBarLabel: "History",
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+        )
+      }
+    },
+    AddEntry: {
+      screen: AddEntry,
+      navigationOptions: {
+        tabBarLabel: "Add Entry",
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome name="plus-square" size={30} color={tintColor} />
+        )
+      }
+    }
+  },
+  {
+    navigationOptions: {
+      header: null
+    },
+    tabBarOptions: {
+      activeTintColor: Platform.OS === "ios" ? purple : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === "ios" ? white : purple,
+        shadowColor: "rgba(0, 0, 0, 0.24)",
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      }
+    }
+  }
+);
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(entries)}>
-      <View style={{flex:1}}>
-        <History />
-      </View>
+        <View style={{ flex: 1 }}>
+          <Tabs />
+        </View>
       </Provider>
     );
   }
